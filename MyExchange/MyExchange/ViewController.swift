5555e7b8c924ce0ea25f18313f707cd59e95983b
Bonța Ioan Cosmin <ioan.bonta@teamextension.ro>
2019-08-09 16:45:42 +0300

    project init

---
+++ MyExchange/MyExchange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyExchange
+//
+//  Created by Cosmin Bonta on 8/9/19.
+//  Copyright © 2019 Cosmin Bonta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
