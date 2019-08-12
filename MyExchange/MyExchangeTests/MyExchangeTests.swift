e55b80fead838e83426095630fc34d826b3da9cd
Bonța Ioan Cosmin <ioan.bonta@teamextension.ro>
2019-08-12 14:26:20 +0300

    added tests

--- MyExchange/MyExchangeTests/MyExchangeTests.swift
+++ MyExchange/MyExchangeTests/MyExchangeTests.swift
@@ -7,12 +7,59 @@
 //
 
 import XCTest
+import RxSwift
+import RxBlocking
+import Nimble
+import RxNimble
+import OHHTTPStubs
 
 
 @testable import MyExchange
 
 class MyExchangeTests: XCTestCase {
 
-   
+    override func setUp() {
+        super.setUp()
 
+        if let path = Bundle.main.path(forResource: "latest", ofType: "json") {
+            stub(condition: isHost("exchangeratesapi.io")) { _ in
+                return OHHTTPStubsResponse(fileAtPath: path, statusCode: 200, headers: nil)
+            }
+        }
+
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        OHHTTPStubs.removeAllStubs()
+    }
+    
+    func testQuote() {
+        
+        let obs = ExchangeAPI.latest(for: "EUR")
+        
+        guard let quote = obs.toBlocking().firstOrNil() else {
+            XCTFail()
+            return
+        }
+        
+        expect(quote.base == "EUR").to(beTrue())
+        expect(quote.rates).toNot(beNil())
+        expect(quote.rates.count > 0).to(beTrue())
+        expect(quote.rates.first?.0).toNot(beNil())
+        expect(quote.rates.first?.1).toNot(beNil())
+    
+        
+    }
+
+}
+
+extension BlockingObservable {
+    func firstOrNil() -> E? {
+        do {
+            return try first()
+        } catch {
+            return nil
+        }
+    }
 }
